dd0a5159ff162e131f1657b6ac69a9edf1d2d629
Sam-Pig <1429204899@qq.com>
2018-05-04 17:40:15 +0800

    add notes

---
+++ reading-notes/JS深入浅出/make-wheels/sticky-module/js/sticky.js
@@ -0,0 +1,29 @@
+// class Sticky{
+// 	constructor(selector,n){
+// 		this.elements = $(selector);
+// 		this.offset = n;
+// 	}
+// }
+
+// var sticky = new Sticky();
+
+
+var topbarOffset = $('#topbar').offset();
+var buttonOffset = $('button').offset();
+
+$(window).on('scroll',function(){
+
+	var scrollY = window.scrollY;
+
+	if(scrollY + 0 > topbarOffset.top){
+		$('#topbar').addClass('sticky');
+	}else{
+		$('#topbar').removeClass('sticky');
+	}
+
+	if(scrollY + 60 > buttonOffset.top){
+		$('button').addClass('sticky')
+	}else{
+		$('button').removeClass('sticky');
+	}
+});
