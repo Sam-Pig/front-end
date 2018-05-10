69dbb415acdfda1be9da07edf55166e7c024f40f
Sam-Pig <1429204899@qq.com>
2018-05-10 19:31:02 +0800

    fix readme

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-<h3>综合项目demo</h3>
-<p>在线todo-list [demo](https://sam-pig.github.io/front-end/projects/online-todo-list/index.html)<p>
+## 综合项目demo
+在线todo-list [demo](https://sam-pig.github.io/front-end/projects/online-todo-list/index.html)
 
-<h3>JS练习demo</h3>
-<p>
- DOM操作练习[demo](https://sam-pig.github.io/front-end/practice/task-js-01.html)
-</p>
-<p>
- 登录注册翻转[demo](https://sam-pig.github.io/front-end/project/flip.html)
-</p>
-<p>todolist[demo](https://sam-pig.github.io/front-end/tasks/todolist-online/static/index.html</p>
+## JS练习demo
+
+DOM操作练习[demo](https://sam-pig.github.io/front-end/practice/task-js-01.html)
+登录注册翻转[demo](https://sam-pig.github.io/front-end/project/flip.html)
+
+todolist[demo](https://sam-pig.github.io/front-end/tasks/todolist-online/static/index.html)
--- pith/deepinJS/make-wheels/sticky-module/js/sticky.js
+++ pith/deepinJS/make-wheels/sticky-module/js/sticky.js
@@ -1,29 +1,38 @@
-// class Sticky{
-// 	constructor(selector,n){
-// 		this.elements = $(selector);
-// 		this.offset = n;
-// 	}
-// }
-
-// var sticky = new Sticky();
-
-
-var topbarOffset = $('#topbar').offset();
-var buttonOffset = $('button').offset();
-
-$(window).on('scroll',function(){
-
-	var scrollY = window.scrollY;
-
-	if(scrollY + 0 > topbarOffset.top){
-		$('#topbar').addClass('sticky');
-	}else{
-		$('#topbar').removeClass('sticky');
+class Sticky{
+	constructor(selector,y){
+		this.elements = $(selector);
+		this.offset = y || 0;
+		this.addPlaceholder();
+		this.cacheOffsets();
+		this.listenToScoll();
 	}
 
-	if(scrollY + 60 > buttonOffset.top){
-		$('button').addClass('sticky')
-	}else{
-		$('button').removeClass('sticky');
+	addPlaceholder(){
+		this.elements.each((index, element)=> {
+			$(element).wrap('<div class="placeholder"></div>');
+			$(element).parent().height($(element).height());
+		});
 	}
-});
+	cacheOffsets(){
+		this.offsets = [];
+		this.elements.each((index, element)=> {
+			this.offsets[index] = $(element).offset();
+		});
+	}
+	listenToScoll(){
+		$(window).on('scroll', ()=> {
+			var scollY = window.scrollY;
+			this.elements.each((index, element)=> {
+				if(scollY + this.offset > this.offsets[index].top){
+					$(element).addClass('sticky')
+							  .css({top: this.offset});
+				}else{
+					$(element).removeClass('sticky');
+				}
+			})
+		});
+	}
+}
+
+new Sticky('#topbar');
+new Sticky('button', 60);
